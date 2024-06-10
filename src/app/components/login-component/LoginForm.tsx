608a8e9d4a4726f6039c85ea020b104ec7fa0c66
Chitin1627 <pratyushprashob@gmail.com>
2024-06-10 17:19:41 +0530

    Created form for login

--- src/app/components/login-component/LoginForm.tsx
+++ src/app/components/login-component/LoginForm.tsx
@@ -1,7 +1,19 @@
+"use client"
+import { useState } from "react"
 import { FaRegEnvelope } from "react-icons/fa"
 import { MdLockOutline } from "react-icons/md"
 
-export default function LoginForm() {
+const LoginForm = () => {
+
+    const [email,setEmail] = useState(""); 
+    const [password, setPassword] = useState(""); 
+    const [loginData, setLoginData] = useState({});
+    const formSubmit = (e: any) => {
+        e.preventDefault()
+        const information = {email: email, password: password}
+        setLoginData(information)
+        console.log(information)
+    }
     return (
         <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
             <div className="bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl overflow-hidden">
@@ -13,23 +25,37 @@ export default function LoginForm() {
                         <h2 className="text-3xl font-bold text-violet-700 mb-2">Log in to account</h2>
                         <div className="border-2 w-10 border-violet-700 inline-block mb-10"></div>
                         <div className="flex flex-col items-center">
-                            <div className="bg-gray-100 w-64 p-2 flex items-center rounded-2xl mb-5">
-                                <FaRegEnvelope className="text-gray-400 m-2"/>
-                                <input type="email" name="email" placeholder="Email" className="bg-gray-100 outline-none text-sm flex-1"/>
-                            </div>
-                            <div className="bg-gray-100 w-64 p-2 flex items-center rounded-2xl mb-5">
-                                <MdLockOutline className="text-gray-400 m-2"/>
-                                <input type="password" name="password" placeholder="Password" className="bg-gray-100 outline-none text-sm flex-1"/>
-                            </div>
-                            <div className="flex w-64 mb-5"> 
-                                <label className="flex items-center text-xs font-light">
-                                    <input type="checkbox" name="remember" className="mr-1"/>
-                                    Remember me
-                                </label>
-                            </div>
-                            <a href="#" className="border-2 border-violet-700 rounded-full px-12 py-2 inline-block font-semibold hover:bg-violet-700 hover:text-white">
-                                Log in
-                            </a>
+                            <form action="" onSubmit={formSubmit}>
+                                <div className="bg-gray-100 w-64 p-2 flex items-center rounded-2xl mb-5">
+                                    <FaRegEnvelope className="text-gray-400 m-2"/>
+                                    <input 
+                                        type="email" 
+                                        name="email" 
+                                        placeholder="Email" 
+                                        className="bg-gray-100 outline-none text-sm flex-1"
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    />
+                                </div>
+                                <div className="bg-gray-100 w-64 p-2 flex items-center rounded-2xl mb-5">
+                                    <MdLockOutline className="text-gray-400 m-2"/>
+                                    <input 
+                                        type="password" 
+                                        name="password" 
+                                        placeholder="Password" 
+                                        className="bg-gray-100 outline-none text-sm flex-1"
+                                        onChange={(e) => setPassword(e.target.value)}
+                                    />
+                                </div>
+                                <div className="flex w-64 mb-5"> 
+                                    <label className="flex items-center text-xs font-light">
+                                        <input type="checkbox" name="remember" className="mr-1"/>
+                                        Remember me
+                                    </label>
+                                </div>
+                                <button type="submit" className="border-2 border-violet-700 rounded-full px-12 py-2 inline-block font-semibold hover:bg-violet-700 hover:text-white">
+                                    Log in
+                                </button>
+                            </form>
                         </div>
                     </div>
                 </div>
@@ -45,3 +71,5 @@ export default function LoginForm() {
         </main>
     )
 }
+
+export default LoginForm;
