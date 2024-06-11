fa8fc58899c451e97a6c4b897762b5c280929419
Chitin1627 <pratyushprashob@gmail.com>
2024-06-11 11:45:58 +0530

    Test push

--- src/app/components/login-component/LoginForm.tsx
+++ src/app/components/login-component/LoginForm.tsx
@@ -12,7 +12,7 @@ const LoginForm = () => {
         e.preventDefault()
         const information = {email: email, password: password}
         setLoginData(information)
-        console.log(information)
+        //console.log(information)
     }
     return (
         <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
