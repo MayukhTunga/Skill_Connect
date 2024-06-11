8a41664919f60f27d1a9c82e14865a8022060923
Chitin1627 <pratyushprashob@gmail.com>
2024-06-11 22:18:01 +0530

    Small change

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "../components/login-component/LoginForm";
 
 export default function page() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-black-100">
+        <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-black">
             <LoginForm />
         </div>
     )
