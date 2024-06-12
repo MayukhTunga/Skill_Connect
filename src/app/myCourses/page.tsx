e6abe4f52b13598313d9a6037d8c17a52b6b921e
s1834 <s18324@protonmail.com>
2024-06-13 03:50:01 +0530

    created myCourses and issue in myCourses screen width

---
+++ src/app/myCourses/page.tsx
@@ -0,0 +1,13 @@
+import MyCourses from "../components/MyCourses";
+import Navbar from "../components/Navbar";
+
+export default function page() {
+  return (
+    <div className="relative min-h-screen bg-black">
+      <Navbar />
+      <div className="flex min-h-screen flex-col items-center justify-between px-24 pt-24 bg-black ">
+        <MyCourses />
+      </div>
+    </div>
+  );
+}
